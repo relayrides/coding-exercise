427759d266d354e4d11543ec08a03bca5992aa2c
Christian Eager <christian@relayrides.com>
2015-10-06 17:07:41 -0700

    Updated exercise options

--- pricing/BasicPricer.java
+++
@@ -1,8 +0,0 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.time.ZonedDateTime;
-
-public interface BasicPricer {
-	
-    BasicQuote getQuote(ZonedDateTime start, ZonedDateTime end, BasicRate rate);
-}
